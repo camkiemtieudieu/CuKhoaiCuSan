1b7113604efff1802847c151989a9c68269ddf83
camkiemtieudieu <30649264+camkiemtieudieu@users.noreply.github.com>
2017-08-15 16:23:38 +0700

    Dung  Autofac để triển khai Dependency Injection

--- CuKhoaiCuSan.Web/Api/ItemController.cs
+++ CuKhoaiCuSan.Web/Api/ItemController.cs
@@ -1,11 +1,16 @@
 ﻿using CuKhoaiCuSan.Web.Infrastructure.Core;
 using System.Collections.Generic;
 using System.Web.Http;
+using CuKhoaiCuSan.Service;
 
 namespace CuKhoaiCuSan.Web.Api
 {
     public class ItemController : ApiControllerBase
     {
+        public ItemController(IErrorService errorService) : base(errorService)
+        {
+        }
+
         // GET api/<controller>
         public IEnumerable<string> Get()
         {
